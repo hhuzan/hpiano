dcbc2c1e299c85ee71de8c264d64eeefe4180e1a
Hugo <hhuzan@gmail.com>
2025-07-25 02:26:14 -0300

    -

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
 	title: "HPiano",
 	description: "Hugo's Piano",
+	icons: {
+		icon: "/favicon.ico",
+	},
 };
 
 export default function RootLayout({ children }) {
