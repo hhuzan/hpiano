cdb664ff8f4d69d3ec16fa13e0b949e1fbae3653
Hugo <hhuzan@gmail.com>
2025-08-12 12:52:58 -0300

    -

--- src/app/api/minutos_por_dia/route.js
+++ src/app/api/minutos_por_dia/route.js
@@ -7,10 +7,10 @@ const sql = neon(`${process.env.DATABASE_URL}`);
 export async function GET() {
 	const result = await sql`
 		SELECT
-			DATE(start_time) AS dia,
+			DATE(start_time AT TIME ZONE 'America/Argentina/Buenos_Aires') AS dia,
 			ROUND(SUM(EXTRACT(EPOCH FROM (end_time - start_time)) / 60)) AS minutos
 		FROM midi_blocks
-		GROUP BY DATE(start_time)
+		GROUP BY DATE(start_time AT TIME ZONE 'America/Argentina/Buenos_Aires')
 		ORDER BY dia;
   `;
 	return NextResponse.json(result);
