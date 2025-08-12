bbc18ae455fdc2e88d3d8573962936fe7fa219ad
Hugo <hhuzan@gmail.com>
2025-08-12 14:20:13 -0300

    -

--- src/app/api/minutos_por_dia/route.js
+++ src/app/api/minutos_por_dia/route.js
@@ -7,10 +7,10 @@ const sql = neon(`${process.env.DATABASE_URL}`);
 export async function GET() {
 	const result = await sql`
 		SELECT
-			TO_CHAR(start_time, 'YYYY-MM-DD') AS dia,
+			TO_CHAR(start_time, 'YY-MM-DD') AS dia,
 			ROUND(SUM(EXTRACT(EPOCH FROM (end_time - start_time)) / 60)) AS minutos
 		FROM midi_blocks
-		GROUP BY TO_CHAR(start_time, 'YYYY-MM-DD')
+		GROUP BY TO_CHAR(start_time, 'YY-MM-DD')
 		ORDER BY dia;
   `;
 	return NextResponse.json(result);
