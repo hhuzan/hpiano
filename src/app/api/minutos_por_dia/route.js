d71bfe6ad06e8b9abab8905b75112bf96b2874b1
Hugo <hhuzan@gmail.com>
2026-03-02 03:25:01 -0300

    -

--- src/app/api/minutos_por_dia/route.js
+++ src/app/api/minutos_por_dia/route.js
@@ -8,15 +8,30 @@ export async function GET() {
 		const sql = neon(process.env.DATABASE_URL);
 
 		const data = await sql`
-  			SELECT
-    		DATE(start_time AT TIME ZONE 'America/Argentina/Buenos_Aires') AS dia,
-    		ROUND(
-  				SUM(EXTRACT(EPOCH FROM (end_time - start_time)) / 60)
-			) AS minutos
-  			FROM midi_blocks
-  			GROUP BY dia
-  			ORDER BY dia ASC
-		`;
+      WITH dias AS (
+        SELECT generate_series(
+          CURRENT_DATE - INTERVAL '89 days',
+          CURRENT_DATE,
+          INTERVAL '1 day'
+        )::date AS dia
+      ),
+      minutos_por_dia AS (
+        SELECT
+          DATE(start_time AT TIME ZONE 'America/Argentina/Buenos_Aires') AS dia,
+          ROUND(
+            SUM(EXTRACT(EPOCH FROM (end_time - start_time)) / 60)
+          ) AS minutos
+        FROM midi_blocks
+        GROUP BY dia
+      )
+      SELECT
+        dias.dia,
+        COALESCE(minutos_por_dia.minutos, 0) AS minutos
+      FROM dias
+      LEFT JOIN minutos_por_dia
+        ON dias.dia = minutos_por_dia.dia
+      ORDER BY dias.dia ASC
+    `;
 
 		const formatted = data.map((row) => ({
 			dia: row.dia.toISOString().slice(0, 10),
