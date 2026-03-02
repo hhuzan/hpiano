6ab3097aa0f8a5d85143271b6bac7c04ffc1a63c
Hugo <hhuzan@gmail.com>
2026-03-02 03:33:31 -0300

    -

--- src/app/api/total/route.js
+++ src/app/api/total/route.js
@@ -1,31 +1,34 @@
 // app/api/bloques/route.js
+
 import { NextResponse } from "next/server";
 import { neon } from "@neondatabase/serverless";
 
-const sql = neon(`${process.env.DATABASE_URL}`);
+const sql = neon(process.env.DATABASE_URL);
 
 export async function GET() {
-	const result = await sql`
-		WITH primer_dia AS (
-			SELECT MIN(
-				date_trunc(
-					'day',
-					start_time AT TIME ZONE 'America/Argentina/Buenos_Aires'
-				)::date
-			) AS dia_inicio
-			FROM midi_blocks
-		),
-		dias AS (
-			SELECT generate_series(
-				(SELECT dia_inicio FROM primer_dia),
-				CURRENT_DATE,
-				INTERVAL '1 day'
-			)::date AS dia
-		)
-		SELECT
-			(SELECT COUNT(*) FROM dias) AS dias,
-			SUM(end_time - start_time) AS tiempo,
-			SUM(note_on_count) AS notas
-		FROM midi_blocks;`;
-	return NextResponse.json(result);
+	try {
+		const result = await sql`
+      WITH primer_dia AS (
+        SELECT MIN(
+          DATE(start_time AT TIME ZONE 'America/Argentina/Buenos_Aires')
+        ) AS dia_inicio
+        FROM midi_blocks
+      )
+      SELECT
+        (
+          CURRENT_DATE -
+          (SELECT dia_inicio FROM primer_dia)
+        ) + 1 AS dias,
+        ROUND(
+          SUM(EXTRACT(EPOCH FROM (end_time - start_time)) / 60)
+        ) AS minutos_totales,
+        SUM(note_on_count) AS notas_totales
+      FROM midi_blocks;
+    `;
+
+		return NextResponse.json(result[0]);
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json({ error: "Error obteniendo resumen de bloques" }, { status: 500 });
+	}
 }
